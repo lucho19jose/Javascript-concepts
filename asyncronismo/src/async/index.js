c506c6ae0e82bc6e27961f08d05c17a548f0e7af
lucho19jose <barbozagonzalesjose@gmail.com>
2022-12-28 16:23:34 -0500

    async

---
+++ asyncronismo/src/async/index.js
@@ -0,0 +1,23 @@
+const fnAsync = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+    ? setTimeout(() => resolve('Async!!'), 2000) 
+    : reject(new Error('Erro!'));
+  })
+}
+
+const anotherFn = async () => {
+  const something = await fnAsync();
+  console.log(something);
+  console.log('Hello!');
+}
+
+console.log('Before');
+anotherFn();
+console.log('After');
+/* 
+Before
+After
+Async!!
+Hello!
+*/
